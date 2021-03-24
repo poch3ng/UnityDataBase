effd33ec7363888b396617593ff7564f0beb0822
poch3ng <poch3ng@gmail.com>
2021-03-24 18:08:54 +0800

    init unity project

---
+++ Assets/GetScore.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class GetScore : MonoBehaviour
+{
+    // 要顯示分數的 Text
+    public Text messageText;
+
+    public void CallRequestScore()
+    {
+        StartCoroutine(RequestScore());
+    }
+
+    IEnumerator RequestScore()
+    {
+        // 從 Database 裡面拿東西, 透過 getScore.php
+        UnityWebRequest request = UnityWebRequest.Get("http://localhost/sqlconnect/getScore.php");
+        yield return request.SendWebRequest();
+
+        if (request.isNetworkError || request.isHttpError)
+        {
+            // Network error
+            Debug.Log(request.error);
+        }
+        else
+        {
+            // request.downloadHandler.text 是PHP回傳的值
+            Debug.Log(request.downloadHandler.text);
+            // 將回傳值放到 Text 裡面
+            messageText.text = "Top Score \n" + request.downloadHandler.text;
+            Debug.Log("Get successfully.");
+        }
+    }
+
+    private void Start()
+    {
+        CallRequestScore();
+    }
+}
---
+++ Assets/Registeration.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class Registeration : MonoBehaviour
+{
+    // 輸入使用者名稱
+    public InputField nameField;
+
+    // 送出按鈕
+    public Button submitBtn;
+
+    public void CallRegister(){
+        StartCoroutine(Register());
+    }
+
+    IEnumerator Register(){
+        WWWForm form = new WWWForm();
+        // 建立要傳到 Database 裡面的資料
+        // username 是 key 值, nameField.text 是 value
+        form.AddField("username", nameField.text);
+
+        // 透過 Post 將資料傳到 PHP
+        UnityWebRequest www = UnityWebRequest.Post("http://localhost/sqlconnect/register.php", form);
+        yield return www.SendWebRequest();
+
+        if (www.isNetworkError || www.isHttpError)
+        {
+            // Network error
+            Debug.Log(www.error);
+        }
+        else if ( www.downloadHandler.text != "0")
+        {
+            // Get Error Like: user already existed
+            Debug.Log(www.downloadHandler.text);
+        }
+        else
+        {
+            Debug.Log("User created successfully.");
+        }
+    }
+
+
+    // Verify Username has input so we enable submit button
+    public void VerifyInput()
+    {
+        submitBtn.interactable = (nameField.text.Length > 0);
+    }
+}
